e14f93de781e696ecbe1583f18cdb38b47cfe131
cfaloon <cfaloon@gmail.com>
2017-02-21 13:10:37 -0500

    Write some User validations and test them.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
-require 'test_helper'
+require "minitest/autorun"
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class TestUser < Minitest::Test
+
+  def test_user_model_validations
+    user = User.new(first_name: nil, last_name: nil)
+    refute user.valid?
+    user = User.new(first_name: 'Bilbo', last_name: 'Baggins', email: 'bbaggins@middleearth.net', password: 'theRing')
+    assert user.valid?
+  end
 end
