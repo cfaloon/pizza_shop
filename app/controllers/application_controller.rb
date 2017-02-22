b4270a2b2431c0e292598de0eb39cc6bdf71b478
cfaloon <cfaloon@gmail.com>
2017-02-22 11:34:55 -0500

    Allow registration without an address.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    keys = %i(email password password_confirmation first_name last_name phone_number)
+    devise_parameter_sanitizer.permit(:sign_up, keys)
+  end
 end
