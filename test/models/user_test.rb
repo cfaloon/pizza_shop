f29c607a3c22ac4d6e61b02bd750cfa574881355
cfaloon <cfaloon@gmail.com>
2017-02-22 11:34:30 -0500

    Relax user validation on address field.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class TestUser < Minitest::Test
   def test_user_model_validations
     user = User.new
     refute user.valid?
-    %i(first_name last_name email password phone_number address).each do |field|
+    %i(first_name last_name email password phone_number).each do |field|
       assert user.errors.keys.include?(field)
     end
     valid_user = User.new(first_name: 'Bill', last_name: 'Smith', email: 'b.smith@domain.com', password: 'starPower', phone_number: '555-562-1010', address: { line_one: '27 Little Ln', line_two: nil, city: 'Durham', state: 'NC', zipcode: '27705' })
