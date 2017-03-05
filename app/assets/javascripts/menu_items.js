8baeafd54de92259e279a88328d4856930296dac
cfaloon <cfaloon@gmail.com>
2017-03-05 15:35:52 -0500

    Remove item from javascript order object.

--- app/assets/javascripts/menu_items.js
+++ app/assets/javascripts/menu_items.js
@@ -4,16 +4,26 @@ function setUserId(userId) {
   order.user_id = userId;
 };
 
-function addItemToOrder(menuItemId, price) {
-  order.order_items.push({ menu_item_id: menuItemId, price: price });
+function addItemToOrder(menuItemId, name, price) {
+  order.order_items.push({ menu_item_id: menuItemId, name: name, price: price });
   buildCartHtml();
 };
 
 function buildCartHtml() {
   var cart = $('#cart');
   var html = $('<ul></ul>');
-  order.order_items.forEach( function(order) {
-    html.append('<li>' + order.menu_item_id + ' - $' + order.price + '</li>');
+  order.order_items.forEach( function(orderItem, index, arr) {
+    html.append('<li>' + orderItem.name + ' - $' + orderItem.price + ' <a onclick="removeItem(' + index + ')">X</a>' + '</li>');
   });
+  var subtotal = order.order_items.reduce(function(acc, order_item) {
+    return acc + order_item.price;
+  }, 0);
+  console.log(subtotal);
+  html.append('<span>Subtotal: ' + subtotal  + '</span>');
   cart.html(html);    
+};
+
+function removeItem(index) {
+  order.order_items.splice(index, 1);
+  buildCartHtml();
 };
