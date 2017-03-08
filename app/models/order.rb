64b5a9a709531d31d4090ae278f124462ccbdd81
cfaloon <cfaloon@gmail.com>
2017-03-08 16:52:15 -0500

    Customize order#show view.

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,8 @@ class Order < ActiveRecord::Base
   has_many :menu_items, through: :order_items
 
   accepts_nested_attributes_for :order_items
+
+  def subtotal
+    menu_items.map(&:price).reduce(&:+)
+  end
 end
