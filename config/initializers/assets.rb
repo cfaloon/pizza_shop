0e7d8eed0147ee3689af34ae44e5f66af4c6d531
cfaloon <cfaloon@gmail.com>
2017-03-09 16:09:48 -0500

    Add a social media side bar on the about page and remove border from cart.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( menu_items.js menu_items.css )
+Rails.application.config.assets.precompile += %w( menu_items.js menu_items.css about.css)
